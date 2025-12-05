d913aa2e9afe4f2b3659ad907d67125490847058
Lighto782 <lighto782@proton.me>
2025-12-05 23:13:19 +0200

    not a lot, made it look nicer, struggling with the functionality

--- My Sulotions/Numbers/Calculator.py
+++ My Sulotions/Numbers/Calculator.py
@@ -2,61 +2,71 @@
  A simple calculator to do basic operators. Make it a scientific calculator for added complexity.
 '''
 
+from decimal import Decimal
+
 from textual import events, on
-from textual.reactive import reactive
+from textual.reactive import reactive, var
 from textual.app import App, ComposeResult
-from textual.containers import Container
-from textual.widgets import Footer, Header, Static, TextArea, Button
+from textual.containers import Container, VerticalGroup
+from textual.widgets import Footer, Header, Static, Button, Digits
 
 class NumberPad(Static):
     '''Custom Number pad'''
     
     def compose(self) -> ComposeResult:
         with Container(id="numpad"):
-            yield Button("9", id="n9")
-            yield Button("8", id="n8")
-            yield Button("7", id="n7")
-            yield Button("(", id="rightB")
-            yield Button(")", id="leftB")
-            yield Button("6", id="n6")
-            yield Button("5", id="n5")
-            yield Button("4", id="n4")
-            yield Button("*", id="multi")
-            yield Button("/", id="divide")
-            yield Button("3", id="n3")
-            yield Button("2", id="n2")
-            yield Button("1", id="n1")
-            yield Button("+", id="add")
-            yield Button("-", id="sub")
-            yield Button("0", id="n0")
-            yield Button(".", id="point")
-            yield Button("+/-", id="neg")
-            yield Button("=", id="equal",  classes="spantwo")
+            yield Button("9", id="n9", classes="number")
+            yield Button("8", id="n8", classes="number")
+            yield Button("7", id="n7", classes="number")
+            yield Button("(", id="lbracket", variant="warning")
+            yield Button(")", id="rbracket", variant="warning")
+            yield Button("6", id="n6", classes="number")
+            yield Button("5", id="n5", classes="number")
+            yield Button("4", id="n4", classes="number")
+            yield Button("*", id="multi", variant="warning")
+            yield Button("/", id="divide", variant="warning")
+            yield Button("3", id="n3", classes="number")
+            yield Button("2", id="n2", classes="number")
+            yield Button("1", id="n1", classes="number")
+            yield Button("+", id="add", variant="warning")
+            yield Button("-", id="sub", variant="warning")
+            yield Button("C", id="reset", variant="error")
+            yield Button("0", id="n0", classes="number")
+            yield Button(".", id="point", variant="primary")
+            yield Button("=", id="equal",  classes="spantwo", variant="success")
+
 
 class Display(Static):
-    text = reactive("Hello")
     
+    def compose(self) -> ComposeResult:
+        with VerticalGroup(id ="screen"):
+            yield Static("0", id="equation")
+            yield Digits("0", id="result")
+        
 
             
 class calculator(App):
     
-    BINDINGS = [("d", "toggle_dark", "Toggle dark mode")]
     
     CSS_PATH = "Calculator.css"
+    
+    numbers = var("0")
     
     def compose(self) -> ComposeResult:
         yield Header(show_clock=True)
         yield Footer()
-        with Container(id="calculator"):
-            yield Display("HHHH", id="screen")
+        with VerticalGroup(id="calculator"):
+            yield Display()
             yield NumberPad()
         
-    def action_toggle_dark(self) -> None:
-        """An action to toggle dark mode."""
-        self.theme = (
-            "textual-dark" if self.theme == "textual-light" else "textual-light"
-        )
         
+    @on(Button.Pressed, ".number")
+    def number_pressed(self, event: Button.Pressed) -> None:
+        """Pressed a number."""
+        assert event.button.id is not None
+        number = event.button.id[-1]
+        sc = self.query_one("#equation", Static)
+        sc.update(number)
 
 
 
