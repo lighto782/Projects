9ae52be37623570fbf2dd1dcd5550756f6417c06
Lighto782 <lighto782@proton.me>
2025-12-08 23:55:55 +0200

    Polished and completed curses functionality, left the themeing and making look nice, next is the real converter

--- My Sulotions/Numbers/UnitConverter.py
+++ My Sulotions/Numbers/UnitConverter.py
@@ -1,11 +1,35 @@
 import curses
 
+class menu():
+    def __init__(self, win, menu: list, selected: int):
+        self.win = win
+        self.menu = menu
+        self.selected = selected
+
+    def draw_menu(self, focused:bool) -> None:
+        self.win.erase()
+        self.win.box()
+        for i, item in enumerate(self.menu):
+                if i == self.selected and focused:
+                    self.win.addstr(i+1, 2, f"> {item}", curses.A_REVERSE | curses.A_ITALIC)
+                else:
+                    self.win.addstr(i+1, 2, item)
+        self.win.refresh()
+    
+    def handle_key(self, key):
+        if key == curses.KEY_UP and self.selected > 0:
+            self.selected -= 1
+        elif key == curses.KEY_DOWN and self.selected < len(self.menu) - 1:
+            self.selected += 1
+        
+
 
 def main(stdscr):
     curses.curs_set(0)
-    stdscr.clear()
-    stdscr.addstr(0,1,"Unit Converter",curses.A_BOLD)
-    stdscr.addstr(1, 3, "Choose which units you want to convert:")
+    stdscr.erase()
+    stdscr.box()
+    stdscr.addstr(1,10,"Unit Converter",curses.A_BOLD)
+    stdscr.addstr(2, 3, "Choose which units you want to convert:")
     menu_items = ["Tempreture (C°, F°, K°)", "Weight (Kg, G, Lb, Ton, Oz, ....)", "Length (M, Km, Ft, Mile, Inch, .....)", "Exit"]
     Units = [
     ["Celsius", "Fahrenheit", "Kelvin"],
@@ -14,126 +38,82 @@ def main(stdscr):
     []
     ]
     
-    whereis = 0
+    UNIT, FROM, TO, INPUT = range(4)
     
-    selected = 0
-    selected_from = 0
-    selected_to = 0
+    current_window = 0
     
-    debug_win = curses.newwin(8, 15, 0, 30)
+    Units_win = menu(curses.newwin(len(Units) + 2, 50, 3, 1), menu_items, 0)
+    From_Unit_win = menu(curses.newwin(10, 15, 11, 5) , Units[0], 0)
+    To_Unit_win = menu(curses.newwin(10, 15, 11, 21), Units[0], 0)
     
-    From_Unit_win = curses.newwin(10, 14, 9, 5)
-    To_Unit_win = curses.newwin(10, 14, 9, 21)
-    Input_Field_win = curses.newwin(3, 14, 20, 5)
-    Output_Field_win = curses.newwin(3, 14, 20, 21)
+    Input_Field_win = curses.newwin(3, 15, 25, 5)
+    Input_Field_win.box()
+    Output_Field_win = curses.newwin(3, 15, 25, 21)
+    Output_Field_win.box()
+    
+    stdscr.refresh()
+    
+    
+    Input_Field_win.refresh()
+    Output_Field_win.refresh()
     
     while True:
-        for i, item in enumerate(menu_items):
-            if i == selected and whereis % 4 == 0:
-                stdscr.move(i+3,1)
-                stdscr.clrtoeol()
-                stdscr.addstr(i+3, 4, f"> {item}", curses.A_REVERSE)
-            else:
-                stdscr.move(i+3,1)
-                stdscr.clrtoeol()
-                stdscr.addstr(i+3, 4, item)
         
-        From_Unit_win.clear()
-        From_Unit_win.box()
-        for j, conv in enumerate(Units[selected]):
-            if j == selected_from and whereis % 4 == 1:
-                From_Unit_win.move(j+1,0)
-                From_Unit_win.clrtoeol()
-                From_Unit_win.addstr(j+1, 1, f"> {conv}", curses.A_REVERSE)
-            else:
-                From_Unit_win.addstr(j+1, 1, f"{conv}")
-        From_Unit_win.refresh()
+        From_Unit_win.menu = Units[Units_win.selected]
+        To_Unit_win.menu = Units[Units_win.selected]
         
-        To_Unit_win.clear()
-        To_Unit_win.box()
-        for k, conv in enumerate(Units[selected]):
-            if k == selected_to and whereis % 4 == 2:
-                To_Unit_win.move(k+1,0)
-                To_Unit_win.clrtoeol()
-                To_Unit_win.addstr(k+1, 1, f"> {conv}", curses.A_REVERSE)
-            else:
-                To_Unit_win.addstr(k+1, 1, f"{conv}")
-        To_Unit_win.refresh()
+        Units_win.draw_menu(current_window == UNIT)
+        From_Unit_win.draw_menu(current_window == FROM)
+        To_Unit_win.draw_menu(current_window == TO)
         
-        Input_Field_win.clear()
-        Input_Field_win.box()
-        Input_Field_win.addstr(1,1,"")
-        Input_Field_win.refresh()
-        
-        Output_Field_win.clear()
-        Output_Field_win.box()
-        Output_Field_win.addstr(1,1,"")
-        Output_Field_win.refresh()
-        
-        debug_win.clear()
-        key = stdscr.getch()
-        
-        if whereis % 4 == 3:
-            curses.curs_set(1)              
-            Input_Field_win.clear()
-            Input_Field_win.box()
-            Input_Field_win.refresh()
 
+        if current_window == INPUT:
+            curses.curs_set(1)
             Input_Field_win.move(1, 1)
 
-            user_text = ""
+            Input_Field_win.refresh()
+
+            user_input = ""
             while True:
                 ch = Input_Field_win.getch()
 
-                if ch in (curses.KEY_ENTER, 10, 13):
+                if ch in (curses.KEY_ENTER, 10, 13,9):
+                    Input_Field_win.addstr(1, 1, ' '*len(user_input))
                     break
+                
                 elif ch in (curses.KEY_BACKSPACE, 127):
-                    if user_text:
-                        user_text = user_text[:-1]
-                        Input_Field_win.move(1, 1)
-                        Input_Field_win.clrtoeol()
-                        Input_Field_win.box()
-                        Input_Field_win.addstr(1,1, user_text)
+                    if user_input:
+                        user_input = user_input[:-1]
+                        Input_Field_win.addstr(1,len(user_input) + 1, ' ')
+                        Input_Field_win.move(1, len(user_input) +1)
                         Input_Field_win.refresh()
-                elif chr(ch).isdigit() or chr(ch) == '.':
-                    user_text += chr(ch)
-                    Input_Field_win.addstr(1,1, user_text)
+                
+                elif chr(ch).isdigit() or (chr(ch) == '.' and '.' not in user_input):
+                    user_input += chr(ch)
+                    Input_Field_win.addstr(1,1, user_input)
                     Input_Field_win.refresh()
 
             curses.curs_set(0)
             
-            whereis += 1
+            current_window = 0
             continue
-            
-        if key == curses.KEY_UP and selected > 0 and whereis % 4 == 0:
-            selected -= 1
-            debug_win.addnstr(str(selected), 1, 1)
-            debug_win.refresh()
-        elif key == curses.KEY_UP and selected_from > 0 and whereis % 4 == 1:
-            selected_from -=1
-            debug_win.addnstr(str(selected), 1, 1)
-            debug_win.refresh()
-        elif key == curses.KEY_UP and selected_to > 0 and whereis % 4 == 2:
-            selected_to -=1
-            debug_win.addnstr(str(selected), 1, 1)
-            debug_win.refresh()
-        elif key == curses.KEY_DOWN and selected < len(menu_items) - 1 and whereis % 4 == 0:
-            selected += 1
-            debug_win.addnstr(str(selected), 1, 1)
-            debug_win.refresh()
-        elif key == curses.KEY_DOWN and selected_from < len(Units[selected]) - 1 and whereis % 4 == 1:
-            selected_from +=1
-            debug_win.addnstr(str(selected), 1, 1)
-            debug_win.refresh()
-        elif key == curses.KEY_DOWN and selected_to < len(Units[selected]) - 1 and whereis % 4 == 2:
-            selected_to +=1
-            debug_win.addnstr(str(selected), 1, 1)
-            debug_win.refresh()
-        elif key == ord('\n'):
-            if selected == len(menu_items) - 1 and whereis % 4 == 0:
+        
+        key = stdscr.getch()
+        
+        if current_window == UNIT:
+            Units_win.handle_key(key)
+        elif current_window == FROM:
+            From_Unit_win.handle_key(key)
+        elif current_window == TO:
+            To_Unit_win.handle_key(key)
+        
+        if key == ord('\n'):
+            if Units_win.selected == len(menu_items) - 1 and current_window == UNIT:
                 break
             else:
-                whereis += 1
+                current_window += 1
+        elif key == 9:
+            current_window += 1
         elif key == ord('q'):
             break
         stdscr.refresh()
